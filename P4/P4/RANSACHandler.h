ce4ce00019fd2a82c15bdb15f6290f94eea14784
Jacobob <57237092+JacobSJ97@users.noreply.github.com>
2022-04-28 12:29:01 +0200

    Update RANSACHandler.h
    Moved RANSACHandler.h

--- P4/P4/RANSACHandler.h
+++ P4/P4/RANSACHandler.h
@@ -4,11 +4,13 @@
 
 #ifndef P4_PROJECT_RANSACHANDLER_H
 #define P4_PROJECT_RANSACHANDLER_H
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <royale.hpp>
 #include <iostream>
 #include <mutex>
 
+#include <pcl/common/impl/io.hpp>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/common/projection_matrix.h>
@@ -30,6 +32,9 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
+#include <RANSACHandler.cpp>
+#include <EMGHandler.cpp>
+#include <Camerahandler.h>
 
 #include <chrono>
 #include <thread>
@@ -43,18 +48,34 @@ typedef pcl::PointXYZ PointT;
 typedef pcl::PointCloud<PointT> PointCloudT;
 
 class RANSACHandler {
-
+    
 public:
-
     pcl::PointCloud<PointT>::Ptr Ptcloud;
+    std::array<float, 3> plane_centerPoint_x;
+    std::array<float, 3> plane_centerPoint_y;
+    std::array<float, 3> plane_centerPoint_z;
+    float box_hight;
+    float box_radius;
+    struct gr_object {
+        string obj;
+        string grasp;
+        float h;
+        float w;
+        float r;
+        float l;
+    };
+    RANSACHandler(pcl::PointCloud<PointT>::Ptr& cloud);
+    double* shape_box(const int nPlanes, const pcl::ModelCoefficients& p1, const pcl::ModelCoefficients& p2, const pcl::ModelCoefficients& p3);
+    void shape_cyl(pcl::ModelCoefficients& cyl, const pcl::ModelCoefficients& coefficients, const pcl::PointCloud<PointT>& cloud);
+    std::array<float, 2> getPointCloudExtremes(const pcl::PointCloud<PointT>& cloud, pcl::PointXYZ center, pcl::PointXYZ direction);
+    float dotProduct(pcl::PointXYZ a, pcl::PointXYZ b);
+    float normPointT(pcl::PointXYZ c);
 
-    RANSACHandler(pcl::PointCloud<PointT> &cloud){
-        Ptcloud = cloud;
-    }
-
-
-
+    float check_cyl(pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, pcl::ModelCoefficients::Ptr coefficients_cylinder);
+    float check_box(pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, pcl::ModelCoefficients::Ptr& coefficients_planes1, pcl::ModelCoefficients::Ptr& coefficients_planes3, pcl::ModelCoefficients::Ptr& coefficients_planes2);
+    float check_sph(pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud, pcl::ModelCoefficients::Ptr coefficients_sph);
 };
+
 
 
 #endif //P4_PROJECT_RANSACHANDLER_H
