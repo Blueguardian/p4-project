9b686b78cd838e9f2f2fdb37304070621ae88ab3
Blueguardian <73152401+Blueguardian@users.noreply.github.com>
2022-06-18 14:16:32 +0200

    Stuff and things :D
    Try this miguel

--- UDP shit/Test files for UDP connection/Client/UDPClientTest.cpp
+++ UDP shit/Test files for UDP connection/Client/UDPClientTest.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <ws2tcpip.h>
 #include <string.h>
+#include <conio.h>
 
 #pragma comment (lib, "ws2_32.lib")
 
 using namespace std;
 
-string micpackage;
+char ch;
 
-string byte0 ;
-string byte1 ;
-string byte2 ;
-string byte3 ;
-string byte4 ;
-string byte5 ;
-string byte6 ;
-string byte7 ;
+uint8_t micpackage[9];
 
-void CreatePack() {
-    micpackage = byte0 + byte1 + byte2 + byte3 + byte4 + byte5 + byte6 + byte7;
-}
-
+uint8_t byte0;
+uint8_t byte1;
+uint8_t byte2;
+uint8_t byte3;
+uint8_t byte4;
+uint8_t byte5;
+uint8_t byte6;
+uint8_t byte7;
+uint8_t byte8;
 
 void main(int argc, char* argv[]) //ability to pass in command line option
 {
@@ -41,7 +40,7 @@ void main(int argc, char* argv[]) //ability to pass in command line option
     // Create a hint structure for the server
     sockaddr_in server;
     server.sin_family = AF_INET;
-    server.sin_port = htons(54000);
+    server.sin_port = htons(8051);
     inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
 
     //socket creation
@@ -50,18 +49,86 @@ void main(int argc, char* argv[]) //ability to pass in command line option
     while (1) {
         //write out to socket
         //string s(argv[1]);
-        cin >> byte0;
-        cin >> byte1;
-        cin >> byte2;
-        cin >> byte3;
-        cin >> byte4;
-        cin >> byte5;
-        cin >> byte6;
-        cin >> byte7;
 
-        CreatePack();
+        ch = _getch();
 
-        int sendOk = sendto(out, micpackage.c_str(), micpackage.size() + 1, 0, (sockaddr*)&server, sizeof(server));
+        if (ch == 's') {
+            //"S" has ben pressed
+            micpackage[0] = 1;
+            micpackage[1] = 0;
+            micpackage[2] = 0;
+            micpackage[3] = 0;
+            micpackage[4] = 0;
+            micpackage[5] = 200;
+            micpackage[6] = 0;
+            micpackage[7] = 0;
+            micpackage[8] = 0;
+
+            cout << "S" << endl;
+        }
+
+        else if (ch == 'd') {
+            //"D" has been pressed
+            micpackage[0] = 1;
+            micpackage[1] = 0;
+            micpackage[2] = 0;
+            micpackage[3] = 0;
+            micpackage[4] = 0;
+            micpackage[5] = 0;
+            micpackage[6] = 200;
+            micpackage[7] = 0;
+            micpackage[8] = 0;
+
+            cout << "D" << endl;
+        }
+
+
+        else if (ch == 'a') {
+            //"D" has been pressed
+            micpackage[0] = 1;
+            micpackage[1] = 0;
+            micpackage[2] = 0;
+            micpackage[3] = 0;
+            micpackage[4] = 0;
+            micpackage[5] = 0;
+            micpackage[6] = 0;
+            micpackage[7] = 0;
+            micpackage[8] = 0;
+
+            cout << "A" << endl;
+        }
+
+        else if (ch == 'z') {
+            //"D" has been pressed
+            micpackage[0] = 1;
+            micpackage[1] = 100;
+            micpackage[2] = 0;
+            micpackage[3] = 0;
+            micpackage[4] = 0;
+            micpackage[5] = 0;
+            micpackage[6] = 0;
+            micpackage[7] = 0;
+            micpackage[8] = 0;
+
+            cout << "z" << endl;
+        }
+
+        else if (ch == 'x') {
+            //"D" has been pressed
+            micpackage[0] = 1;
+            micpackage[1] = 0;
+            micpackage[2] = 100;
+            micpackage[3] = 0;
+            micpackage[4] = 0;
+            micpackage[5] = 0;
+            micpackage[6] = 0;
+            micpackage[7] = 0;
+            micpackage[8] = 0;
+
+            cout << "x" << endl;
+        }
+
+        int sendOk = sendto(out, (char*)micpackage, 9, 0, (sockaddr*)&server, sizeof(server));
 
         if (sendOk == SOCKET_ERROR) {
             cout << "failure" << WSAGetLastError() << endl;
