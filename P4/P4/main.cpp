6efb9f828a4e58e385d571bd546fd3ac2c1536ff
Blueguardian <73152401+Blueguardian@users.noreply.github.com>
2022-06-18 08:42:01 +0200

    Update main.cpp

--- P4/P4/main.cpp
+++ P4/P4/main.cpp
@@ -1,21 +1,7 @@
-//
-// Created by mogens on 25/04/2022.
-//
-
 #define _CRT_SECURE_NO_WARNINGS
 
-
-
-//#include <EMGHandler.cpp>
 #include "Camerahandler.h"
-
 #include <conio.h>
-
-// <winsock2.h>
-//#include <ws2tcpip.h>
-//#include <stdio.h>
-
-//#pragma comment (lib, "ws2_32.lib")
 
 using namespace royale;
 using namespace sample_utils;
